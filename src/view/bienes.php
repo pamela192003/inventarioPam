1e7bedd0a57b32f174732ecc427702332cf83fe5
wilmerMendoza <rojasataupillcopamels@gmail.com>
2025-07-25 09:06:54 -0500

    t

--- src/view/bienes.php
+++ src/view/bienes.php
@@ -42,7 +42,9 @@
         </div>
         <div class="card">
             <div class="card-body">
-                <a href="<?=BASE_URL ?>reporte-bienes" class="btn btn-outline-warning"><i class="fa fa-file excel"></i></a>
+                <a href="<?=BASE_URL ?>reporte-bienes" class="btn btn-outline-warning">Imprimir Reporte<i class="fa fa-file excel"></i></a>
+                <a href="<?=BASE_URL ?>reporte-bienes" class="btn btn-outline-warning">Imprimir PDF<i class="fa fa-file excel"></i></a>
+                
                 <h4 class="card-title">Resultados de Búsqueda</h4>
                 <div id="filtros_tabla_header" class="form-group  row page-title-box d-flex align-items-center justify-content-between m-0 mb-1 p-0">
                     <input type="hidden" id="pagina" value="1">
