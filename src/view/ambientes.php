b714d9c845f43c490eb3e1687016a778e2b20429
pamela192003 <rojasataupillcopamels@gmail.com>
2025-07-27 22:58:29 -0500

    27

--- src/view/ambientes.php
+++ src/view/ambientes.php
@@ -36,7 +36,7 @@
         <div class="card">
             <div class="card-body">
                 <a href="<?= BASE_URL ?>imprimir-ambiente" class="btn btn-danger waves-effect waves-light">
-                    <i class="fa fa-file-pdf"></i>PDF
+                    <i class="fa fa-file-pdf"></i>Imprimir PDF
                 </a>
                 <h4 class="card-title">Resultados de Búsqueda</h4>
                 <div id="filtros_tabla_header" class="form-group  row page-title-box d-flex align-items-center justify-content-between m-0 mb-1 p-0">
